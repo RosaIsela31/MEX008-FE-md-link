7d66d59b58239431b2ecc3de69d43f89b66fba48
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-08-29 17:54:50 -0500

    modificando README.md personal

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+// console.log("Holaa");
+
+// module.exports = () => {
+//   // ...
+// };
+
+// Debería retornar que es una función 
+// Debería retornar una promesa 
+// Debería marcar error si no recibe argumentos
+// Debería de recibir una ruta absoluta o convertirla a ruta absoluta
+// Debería identificar si esta en un archivo o directorio
+// Debería identificar el archivo es .md 
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,13 @@
 const mdLinks = require('../index.js');
 
+// A. Un test que me compruebe que es una función 
+// B. Un test que me compruebe que se esta recibiendo una promesa
+// C. 
+
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should be a function', () => {
+    expect(typeof mdLinks).toBe('function');
   });
-
 });
