4dda5148a2dde47d1fcb4c9726db6abba560c975
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.59>
2019-09-09 09:19:17 -0500

    Agregando función que valida el status del url 400, en el archivo validateUrls.js

---
+++ validateUrls.js
@@ -0,0 +1,23 @@
+let counterFail = 0;
+
+const validateUrls = async (result) => {
+  const myPromise = new Promise((resolve) => {
+
+    result.forEach(element => {
+      if (element.status >= 400) {
+        console.log(`${(element.status)}` + ' ' + `${(element.link)}` + `${(' fail ')}` + `${('title: ' + element.text2)}`)
+        counterFail++;
+      } else {
+        console.log(`${(element.status)}` + ' ' + `${(element.link)}` + `${(' ok ')}` + `${('title: ' + element.text2)}`)
+      }
+
+    })
+    resolve(counterFail)
+    const total = result.length
+    console.log('Total: ' + total)
+})
+  const counterResult = await myPromise;
+  return counterResult
+}
+
+module.exports = validateUrls;
