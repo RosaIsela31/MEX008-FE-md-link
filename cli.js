8b0b821d0b17183a5b551920315850a65519c226
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.59>
2019-09-09 09:27:01 -0500

    Agregando las condicionales en el archivo cli.js

--- cli.js
+++ cli.js
@@ -1,1 +1,33 @@
 #!/usr/bin/env node
+const findFilesMD = require('./findFilesMD');
+const readFilesMD = require('./readFilesMD');
+const findUrls = require('./findUrls');
+const statusCode = require('./statusCode');
+const validate = require('./validateUrls');
+const path1 = process.argv[2];// apuntando al archivo que ingrese el usuario
+// process.argv es una matriz que contiene los argumentos de la línea de comandos
+
+if (process.argv[3] == '--validate') {
+  findFilesMD(path1).then((result) => {
+    result.forEach(element => {
+      readFilesMD(element)
+        .then(findUrls)
+        .then(statusCode)
+        .then(validate)
+        .catch(err => (err))
+    });
+  });
+}
+else {
+  findFilesMD(path1).then((result) => {
+    result.forEach(element => {
+      readFilesMD(element)
+        .then(findUrls)
+        .then((data) => {
+          statusCode(data)
+          console.log((data))
+        })
+        .catch(err => (err))
+    })
+  })
+}
